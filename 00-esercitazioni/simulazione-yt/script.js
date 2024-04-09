0d3300a656e072c8adb6e88ae79cedb7066c7112
Federico <federico.izzop@gmail.com>
2024-04-09 15:47:24 +0200

    js funzione

--- 00-esercitazioni/simulazione-yt/script.js
+++ 00-esercitazioni/simulazione-yt/script.js
@@ -15,7 +15,30 @@ function appariMenu() {
 function creaVideo() {
     let contenitore = document.createElement("div");
     contenitore.setAttribute('id', 'contenitoreVideo');
+
+    let immagine = document.createElement("img");
+    immagine.setAttribute('id','thumbnail');
+
+    let titolo = document.createElement("h3");
+    let sottotitolo = document.createElement("p");
+
+    document.getElementById("home").appendChild(contenitore);
+    contenitore.appendChild(immagine).src = "img/video.png";
+    immagine.width = "500";
+    contenitore.appendChild(titolo).innerHTML = "Titolo del video";
+    contenitore.appendChild(sottotitolo).innerHTML = "Sottotitolo del video";
+
+
+
     console.log(contenitore);
+    console.log(immagine);
+    console.log(titolo);
+    console.log(sottotitolo);
+
 }
 
 creaVideo();
+
+for (let i = 0; i < 10; i++) {
+    creaVideo();
+}
