d257f489bec567997a2e3e50b3739a7605f7822d
Federico <federico.izzop@gmail.com>
2024-04-10 11:12:12 +0200

    array

--- 00-esercitazioni/simulazione-yt/script.js
+++ 00-esercitazioni/simulazione-yt/script.js
@@ -11,8 +11,23 @@ function appariMenu() {
     } 
 }
 
+const titoli = [
+    "video 0",
+    "video 1",
+    "video 2",
+    "video 3",
+    "video 4",
+    "video 5",
+    "video 6",
+    "video 7",
+    "video 8",
+    "video 9",
+    "video 10", 
+];
 
 function creaVideo() {
+    for (let i = 0; i < 30; i++) {
+    
     let contenitore = document.createElement("div");
     contenitore.setAttribute('id', 'contenitoreVideo');
 
@@ -25,11 +40,9 @@ function creaVideo() {
     document.getElementById("home").appendChild(contenitore);
     contenitore.appendChild(immagine).src = "img/video.png";
     immagine.width = "500";
-    contenitore.appendChild(titolo).innerHTML = "Titolo del video";
+    contenitore.appendChild(titolo).innerHTML = titoli[i];
     contenitore.appendChild(sottotitolo).innerHTML = "Sottotitolo del video";
-
-
-
+    }
     console.log(contenitore);
     console.log(immagine);
     console.log(titolo);
@@ -39,6 +52,21 @@ function creaVideo() {
 
 creaVideo();
 
-for (let i = 0; i < 10; i++) {
-    creaVideo();
+
+
+// Array
+
+
+
+console.log(titoli[1]);
+
+function test() {
+    for (let i = 0; i < 10; i++) {
+        let divTest = document.createElement("div");
+        document.getElementById("test").appendChild(divTest);
+        divTest.innerHTML = titoli[i];
+    }
 }
+
+test();
+
